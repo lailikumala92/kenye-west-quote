fccb2c8049b844f4f0732857be812c7ba11d5b84
lailikumala92 <lailikumala92@gmail.com>
2021-01-31 13:04:26 -0800

    refresh quote

--- src/App.js
+++ src/App.js
@@ -6,41 +6,55 @@ import { Provider } from "react-redux";
 import { useDispatch, useSelector } from 'react-redux';
 import { GetQuote } from './redux/actions/Users';
 
+
+
 const Content = () => {
-  const dispatch = useDispatch()
-  // const [quote, setQuote] = React.useState("");
- 
-  const  {quote}  = useSelector((s)=> s.Quote)
+
+  const dispatch = useDispatch();
+  const  {quote}  = useSelector((s)=> s.Quote);
+  const [newQuote, setNewQuote] = React.useState("");
 
   React.useEffect(()=> {
     dispatch(GetQuote());    
   }, []);
 
+  const handleChange = e => {
+    setNewQuote(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault(e);
+    console.log(newQuote)
+  };
  
 
   return (
     <>
-    <div className="container-fluid d-flex justify-content-center">
+    <div className="containerd-flex justify-content-center">
       <div className="d-flex flex-column mt-5">
-        <div>
-          <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo1rT-4-v9r60q006kYCPbtAJN2gjDLGpdZA&usqp=CAU"/>  
+        <a className="ml-5" href="https://kanye.rest/">API SITE</a>
+        <div className="d-flex justify-content-center">
+          <img style={{marginTop: 60}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo1rT-4-v9r60q006kYCPbtAJN2gjDLGpdZA&usqp=CAU"/>  
         </div>
-        <h2>Kenye-West Quote</h2>
-        <div className="mt-2 mb-2" style={{fontWeight: 'bold'}}>{quote.quote}</div>
+        <h2 className="d-flex justify-content-center">Kenye-West Quote</h2>
+        <div className="mt-2 mb-2 d-flex justify-content-center" style={{fontWeight: 'bold', fontSize: 20}}>{quote.quote}</div>
         <div className="row d-flex justify-content-center">
           <button width="30" className="rounded"
-          
+          onClick={() => dispatch(GetQuote())}
           >Get Quote</button>
           <button width="30" className="rounded">Add Favorite</button>
         </div>
-        <span style={{width: 300, height: 1, background: "#aaa", marginTop: 30}}></span>
+        <div className="d-flex justify-content-center">
+          <span style={{width: 300, height: 1, background: "#aaa", marginTop: 30}}></span>
+        </div> 
         <h4 className="d-flex justify-content-center mt-5">My Quotes</h4>
-        <form>
+        <form onSubmit={onSubmit}>
           <div  className="d-flex justify-content-center mt-4">
-            <input type="text" name="quote" height="5"/>
-            <button type="submit" height="5" className="rounded">submit</button>
+            <input type="text" id="textinput" onChange={handleChange} />
+            <button type="submit" className="rounded">Submit</button>
           </div>
         </form>
+        <h6 className="d-flex justify-content-center mt-2">{newQuote}</h6>
       </div>
     </div>
   </>
